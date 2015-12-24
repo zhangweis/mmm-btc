fae42b5f4d4fcc6aa27376384cf176ca482b4d11
zhangwei <cdzhangw@gmail.com>
2015-12-24 13:48:47 +0800

    accumulate interest

--- app/app.component.js
+++ app/app.component.js
@@ -1,8 +1,18 @@
 import {Component} from 'angular2/core';
+import {Http} from 'angular2/http';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }
+export class AppComponent { 
+    static get parameters() {
+      return [[Http]];  
+    }
+    constructor(http) {
+        this.http = http;
+        http.get('https://testnet.blockexplorer.com/api/txs/?address=mnph5g44T7uzYahGzx7s1eX1wYWqAjBK5r')
+            .subscribe(txs => console.log(txs.json().txs));
+    }
+}
 
