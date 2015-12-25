e3c12453bf173bfe93db65aa4b4a26834fa663d3
zhangwei <cdzhangw@gmail.com>
2015-12-25 10:54:24 +0800

    add zone.js

--- app/app.component.js
+++ app/app.component.js
@@ -1,48 +1,42 @@
 import {bootstrap, Component, View, Inject, NgZone} from 'angular2/core';
 import {Http} from 'angular2/http';
+import {Account} from './account';
+import lodash from 'lodash';
 
 @Component({selector: 'my-app'})
 @View({
   template: `
-    <h1>people</h1>{{name}}
+    <h1>people</h1>
     <ul class="people">
-      <li *ngFor="#person of people">
-        hello, {{person.name}}
+      <li *ngFor="#account of accounts">
+        {{account.address}} has {{getBalance(account)}}
       </li>
     </ul>
   `
 })
 export class AppComponent {
-  static get parameters() {
-    return [[Http], [NgZone]];  
-  }
-  constructor(http, zone) {
-var self = this;
-zone = new NgZone({enableLongStackTrace:true});
-self.name='old';
-      var got = http.get('./people.json');
-    got.subscribe(
-        people => {
-        // zone.run(function(){
-            self.people = people.json();
-        // });
-        }
-    );
-    setTimeout(() => {
-        zone.run(() =>{ 
-        self.name='changed';
-        self.people = [
-            {
-                name:'1'
-            },
-            {
-                name:'2'
-            }
-            ];
-            
-            // self.ref.markForCheck();
-        });
-    });
-
-  }
+    static get parameters() {
+        return [[Http], [NgZone]];  
+    }
+    constructor(http, zone) {
+        var self = this;
+        self.accounts=[];
+        http.get('https://testnet.blockexplorer.com/api/txs/?address=mnph5g44T7uzYahGzx7s1eX1wYWqAjBK5r')
+            .subscribe(res => {
+                var txs = res.json();
+                var accounts = {};
+                txs.txs.forEach(function(tx){
+                    var sender = Account.getSender(tx);
+                    var account = accounts[sender];
+                    if (!account) {
+                        account = accounts[sender] = new Account(sender);
+                    }
+                    account.addBtcTx(tx);
+                })
+                self.accounts = lodash.values(accounts);
+            });
+    }
+    getBalance(account) {
+        return account.getBalance(new Date());
+    }
 }
